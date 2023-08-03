a538b5e7c123a1ad85d8959bb38523ba72601dc6
Jakub Pečenka <pecenja2@fit.cvut.cz>
2023-08-03 09:42:36 +0200

    add comments and format remap to <leader>=

--- lua/pecenka/remap.lua
+++ lua/pecenka/remap.lua
@@ -1,8 +1,10 @@
 -- https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen/remap.lua
 vim.g.mapleader = " "
+
+-- exit to project
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
-
+-- copy to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>y", [["+y]])
 
@@ -10,20 +12,27 @@ vim.keymap.set("n", "<leader>y", [["+y]])
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- exit insert mode to normal mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- delete or paste over and keep buffered
 vim.keymap.set("x", "<leader>P", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- format whole document
+vim.keymap.set("n", "<leader>=", vim.lsp.buf.format)
 
+-- move selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- save by ctr + s
 vim.keymap.set("n", "<C-s>", ":w<Enter>")
 
+-- windows adjustments
 vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
 vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
 vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
