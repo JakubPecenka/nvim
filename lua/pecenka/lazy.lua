8c5456498b41211bbefc1044528356369dde2880
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-02-12 14:06:22 +0100

    add lazy

---
+++ lua/pecenka/lazy.lua
@@ -0,0 +1,21 @@
+require("lazy").setup({
+  spec = "pecenka.plugins",
+  change_detection = { notify = false },
+  ui = {
+    icons = {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+})
---
+++ lua/pecenka/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+	  'nvim-telescope/telescope.nvim', 
+    tag = '0.1.5',
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          vimgrep_arguments = {
+            "grep",
+            "--extended-regexp",
+            "--color=never",
+            "--with-filename",
+            "--line-number",
+            "-b",
+            "--ignore-case",
+            "--recursive",
+          }
+        }
+      })
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set('n', '<leader>fe', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+      end)
+  end
+}
