ef8e0bd312e2c587582acc833829b744b7448aa0
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-02-13 16:30:30 +0100

    add some files to enable copilot

--- lua/pecenka/plugins/copilot.lua
+++ lua/pecenka/plugins/copilot.lua
@@ -2,6 +2,8 @@ return {
   "github/copilot.vim",
 
   config = function()
+    vim.g.copilot_filetypes = {markdown = true, yaml = true, json = true, toml = true }
+
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     vim.api.nvim_set_keymap("i", "<A-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
