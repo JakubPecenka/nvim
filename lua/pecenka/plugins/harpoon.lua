b32bae30d41143f961d9c4863f0e9d49e14b3c9a
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-02-17 13:43:27 +0100

    update harpoon1 to harpoon2

--- lua/pecenka/plugins/harpoon.lua
+++ lua/pecenka/plugins/harpoon.lua
@@ -1,21 +1,20 @@
 return {
-  'theprimeagen/harpoon',
-  config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function ()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<leader>a", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-    vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<C-z>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-x>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-c>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-v>", function() harpoon:list():select(4) end)
 
-    vim.keymap.set("n", "<C-z>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<C-x>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<C-c>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-v>", function() ui.nav_file(4) end)
-
-    vim.keymap.set("n", "<A-z>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<A-x>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<A-c>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<A-v>", function() ui.nav_file(4) end)
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
   end
 }
