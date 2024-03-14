929190733ed1855671f604aa103ab5d39b4c0fbb
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-03-14 12:43:05 +0100

    update: mapping for copilot

--- lua/pecenka/plugins/copilot.lua
+++ lua/pecenka/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
     vim.g.copilot_filetypes = {markdown = true, yaml = true, json = true, toml = true }
 
     vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    vim.api.nvim_set_keymap("i", "<A-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-b>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<A-b>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end
 }
