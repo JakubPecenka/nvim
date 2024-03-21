5de1468961545c9bae7d138479c709daed9eeb38
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-03-21 12:47:15 +0100

    update: some key mapping

--- lua/pecenka/plugins/copilot.lua
+++ lua/pecenka/plugins/copilot.lua
@@ -5,7 +5,8 @@ return {
     vim.g.copilot_filetypes = {markdown = true, yaml = true, json = true, toml = true }
 
     vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-b>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    vim.api.nvim_set_keymap("i", "<A-b>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end
 }
+
+
