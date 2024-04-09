177931a03654fa77af928984932b8f9a553d7c4c
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-04-09 12:30:31 +0200

    feat: find hidden files

--- lua/pecenka/plugins/telescope.lua
+++ lua/pecenka/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-	  'nvim-telescope/telescope.nvim', 
+	  'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -22,6 +22,9 @@ return {
       })
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fh', function ()
+        builtin.find_files({ hidden = true })
+      end)
       vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
       vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
