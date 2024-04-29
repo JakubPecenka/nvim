4ac89f8a6c9687887643907ec75a7fe2fe9371f9
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-04-29 21:08:54 +0200

    uff

--- lua/pecenka/plugins/colors.lua
+++ lua/pecenka/plugins/colors.lua
@@ -1,38 +1,38 @@
 function ColorMyPencils(color)
 	--color = color or "tokyonight"
 	color = color or "gruvbox"
-  vim.cmd.colorscheme(color)
+	vim.cmd.colorscheme(color)
 
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-
 return {
-  { "ellisonleao/gruvbox.nvim", 
-    name = 'gruvbox',
-    config = function()
-      require('gruvbox').setup({
-        -- disable_background = true,
-        contrast = "hard", -- canbe "hard", "soft" or empty string
-      })
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		config = function()
+			require("gruvbox").setup({
+				-- disable_background = true,
+				contrast = "hard", -- canbe "hard", "soft" or empty string
+			})
 
-      vim.cmd("colorscheme gruvbox")
+			vim.cmd("colorscheme gruvbox")
 
-      ColorMyPencils()
-    end
-  },
-  { "folke/tokyonight.nvim",
-    name = 'tokyonight',
-    config = function()
-      require('tokyonight').setup({
-        style = "night",
-        disable_background = true
-      })
+			ColorMyPencils()
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				disable_background = true,
+			})
 
-      --vim.cmd("colorscheme tokyonight")
-      --ColorMyPencils()
-    end
-  },
+			--vim.cmd("colorscheme tokyonight")
+			--ColorMyPencils()
+		end,
+	},
 }
