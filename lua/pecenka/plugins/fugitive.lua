5c0dd1e9e8ad25e42e4d97d1c2e58d24a24ed9de
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-05-09 09:55:46 +0200

    feat: remap foor fugitive

--- lua/pecenka/plugins/fugitive.lua
+++ lua/pecenka/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "gy", "<cmd>diffget //2<CR>")
-    vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
+    vim.keymap.set("n", "gq", "<cmd>diffget //2<CR>")
+    vim.keymap.set("n", "gp", "<cmd>diffget //3<CR>")
   end
 }
