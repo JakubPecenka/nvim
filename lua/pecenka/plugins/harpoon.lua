6f9552fb672b70f5d44f90b722d433308b525371
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-05-09 09:55:20 +0200

    fix: harpoon mapping didn't work

--- lua/pecenka/plugins/harpoon.lua
+++ lua/pecenka/plugins/harpoon.lua
@@ -8,10 +8,17 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+    -- using C-2 doesn't work as i use C-space for tmux prefix
+    -- https://www.reddit.com/r/tmux/comments/103clhv/space_is_now_bound_to_2_for_some_reason/
+    vim.keymap.set("n", "<C-z>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-x>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-c>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-v>", function() harpoon:list():select(4) end)
+
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
