e4bcb75f256b71012df09a6c140cf1393e46bd73
Jakub Pecenka <jakub.pecenka@cgi.com>
2024-09-16 15:57:30 +0200

    feat: add some remaps for telescope

--- lua/pecenka/plugins/telescope.lua
+++ lua/pecenka/plugins/telescope.lua
@@ -23,20 +23,38 @@ return {
 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
       vim.keymap.set('n', '<leader>fh', function ()
         builtin.find_files({ hidden = true })
       end)
+
       vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+
       -- find given string in files, project wide
       vim.keymap.set('n', '<leader>fe', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
       end)
+
       -- find string under cursor in files, project wide
       vim.keymap.set('n', '<leader>fu', builtin.grep_string, {})
+
+      -- find WORD under cursor in files, project wide
+      vim.keymap.set('n', '<leader>fU', function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+      end)
+
       -- find given string in files, project wide and respect .gitignore
       vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
+
+      -- find given string in files and ignore node_modules
+      vim.keymap.set('n', '<leader>fn', function()
+        builtin.live_grep({ grep_open_files = true, search = vim.fn.input("Grep > "), search_dirs = { '!.git', '!node_modules' } });
+      end)
+
       -- find every occurence of the word under the cursor in the current buffer
       vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+
       -- searching for provided string in the current buffer
       vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
 
