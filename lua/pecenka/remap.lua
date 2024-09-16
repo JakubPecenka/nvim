6fd3927fbaf8a97a62cf8724cd1ab19a1ee89409
Jakub Pecenka <jakub.pecenka@cgi.com>
2024-09-16 16:00:18 +0200

    refactor: just refactoring, more or less

--- lua/pecenka/remap.lua
+++ lua/pecenka/remap.lua
@@ -1,6 +1,9 @@
 -- https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen/remap.lua
 vim.g.mapleader = " "
 
+-- format
+vim.keymap.set({ "n", "v" }, "<leader>f=", vim.lsp.buf.format)
+
 -- exit to project
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
@@ -14,12 +17,9 @@ vim.keymap.set("n", "<leader>§§", ":Copilot enable<CR>")
 -- to keep in the middle when up-and-downing
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<A-d>", "<C-d>zz")
-vim.keymap.set("n", "<A-u>", "<C-u>zz")
 
 -- exit insert mode to normal mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("i", "<A-c>", "<Esc>")
 
 -- replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -34,7 +34,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- save by ctr + s
 vim.keymap.set("n", "<C-s>", ":w<Enter>")
-vim.keymap.set("n", "<A-s>", ":w<Enter>")
 
 -- windows adjustments
 vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]])   -- make the window biger vertically
@@ -43,13 +42,11 @@ vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window b
 vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
 
 -- navigating quickfix list
--- vim.keymap.set("n", "<C-j>", ":cnext<CR>")
--- vim.keymap.set("n", "<C-k>", ":cprev<CR>")
 vim.keymap.set("n", "]q", ":cnext<CR>")
 vim.keymap.set("n", "[q", ":cprev<CR>")
+vim.keymap.set("n", "<leader>qq", ":copen<CR>")
 
 -- do so
-vim.keymap.set("n", "<leader><leader>", function ()
-  vim.cmd("so")
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
 end)
-
