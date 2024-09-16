950ed55356331fc4443567de73928bf2603f0101
Jakub Pecenka <jakub.pecenka@cgi.com>
2024-09-16 15:55:23 +0200

    feat: harpoon remaps

--- lua/pecenka/plugins/harpoon.lua
+++ lua/pecenka/plugins/harpoon.lua
@@ -2,10 +2,10 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function ()
+  config = function()
     local harpoon = require("harpoon")
     harpoon:setup()
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     -- using C-2 doesn't work as i use C-space for tmux prefix
@@ -15,8 +15,9 @@ return {
     vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+    vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():replace_at(2) end)
+    vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():replace_at(3) end)
+    vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():replace_at(4) end)
   end
 }
