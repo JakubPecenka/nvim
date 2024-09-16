03fa6e397003fb7c2331d831c460aba61d21dd62
Jakub Pecenka <jakub.pecenka@cgi.com>
2024-09-16 15:59:01 +0200

    feat: update to new trouble

--- lua/pecenka/plugins/trouble.lua
+++ lua/pecenka/plugins/trouble.lua
@@ -1,40 +1,46 @@
 return {
-  {
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup({
-        icons = false,
-        fold_open = "v", -- icon used for open folds
-        fold_closed = ">", -- icon used for closed folds
-        indent_lines = false, -- add an indent guide below the fold icons
-        signs = {
-          -- icons / text used for a diagnostic
-          error = "error",
-          warning = "warn",
-          hint = "hint",
-          information = "info"
-        },
-      })
-
-      vim.keymap.set("n", "<leader>tt", function()
-        require("trouble").toggle()
-      end)
-
-      vim.keymap.set("n", "<leader>tw", function()
-        require("trouble").toggle("workspace_diagnostics")
-      end)
-
-      vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end)
-
-      vim.keymap.set("n", "]t", function()
-        require("trouble").next({skip_groups = true, jump = true});
-      end)
-
-      vim.keymap.set("n", "[t", function()
-        require("trouble").previous({skip_groups = true, jump = true});
-      end)
-
-
-    end
-  },
+	"folke/trouble.nvim",
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>ts",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>tl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>tL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>tq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+		{
+			"[t",
+			"<cmd>Trouble diagnostics prev<cr><cmd>Trouble diagnostics jump<cr>",
+			desc = "Previous (Trouble)",
+		},
+		{
+			"]t",
+			"<cmd>Trouble diagnostics next<cr><cmd>Trouble diagnostics jump<cr>",
+			desc = "Next (Trouble)",
+		},
+	},
 }
