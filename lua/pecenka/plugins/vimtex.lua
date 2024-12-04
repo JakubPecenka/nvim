1fd446a3872ff101b705b814579960ad03fab997
Jakub Pečenka <pecenja2@fit.cvut.cz>
2024-12-04 13:55:19 +0100

    feat: add vimtex

---
+++ lua/pecenka/plugins/vimtex.lua
@@ -0,0 +1,23 @@
+return {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    vim.g.vimtex_view_method = 'general'
+    vim.g.vimtex_view_general_viewer = 'open'
+    vim.g.vimtex_view_general_options = ''
+
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      executable = 'docker-latex.sh',
+      options = {
+        -- '-xelatex',
+        '-pdf',
+        -- '-interaction=nonstopmode',
+        -- '-synctex=1',
+      }
+    }
+
+  end
+}
+
