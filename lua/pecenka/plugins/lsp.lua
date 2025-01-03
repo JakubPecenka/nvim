8246d5030602c584861b3e35cf5f7b5833ed345c
Jakub Pečenka <pecenja2@fit.cvut.cz>
2025-01-03 16:06:25 +0100

    style: formatting

--- lua/pecenka/plugins/lsp.lua
+++ lua/pecenka/plugins/lsp.lua
@@ -1,73 +1,73 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
 
-        -- https://www.youtube.com/watch?v=FmHhonPjvvA
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
+		-- https://www.youtube.com/watch?v=FmHhonPjvvA
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+	},
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "stylua",
-                "gofumpt",
-                "goimports",
-                "clang-format",
-                "prettier",
-                "black",
-                "isort",
-            },
-        })
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "pyright",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"stylua",
+				"gofumpt",
+				"goimports",
+				"clang-format",
+				"prettier",
+				"black",
+				"isort",
+			},
+		})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"pyright",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-
-                ["pyright"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.pyright.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            python = {
-                                analysis = {
-                                    autoSearchPaths = true,
-                                    useLibraryCodeForTypes = true,
-                                    diagnosticMode = "workspace",
-                                    typeCheckingMode = "basic",
-                                },
-                            },
-                        },
-                    })
-                end,
+				["pyright"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.pyright.setup({
+						capabilities = capabilities,
+						settings = {
+							python = {
+								analysis = {
+									autoSearchPaths = true,
+									useLibraryCodeForTypes = true,
+									diagnosticMode = "workspace",
+									typeCheckingMode = "basic",
+								},
+							},
+						},
+					})
+				end,
 
 				["lua_ls"] = function()
 					local lspconfig = require("lspconfig")
@@ -85,49 +85,51 @@ return {
 			},
 		})
 
-        require("lspconfig").sourcekit.setup {
-          root_dir = function(fname)
-            return require('lspconfig.util').root_pattern('.git', 'Package.swift', 'Makefile', 'project.pbxproj')(fname) or vim.fn.getcwd()
-          end,
+		require("lspconfig").sourcekit.setup({
+			root_dir = function(fname)
+				return require("lspconfig.util").root_pattern(".git", "Package.swift", "Makefile", "project.pbxproj")(
+					fname
+				) or vim.fn.getcwd()
+			end,
 
-          capabilities = capabilities,
-        }
+			capabilities = capabilities,
+		})
 
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            end,
-          },
-          mapping = {
-            ['<CR>'] = cmp.mapping.confirm({select = true}),
-            ['<C-y>'] = cmp.mapping.confirm({select = true}),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-            ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-            ['<C-p>'] = cmp.mapping(function()
-              if cmp.visible() then
-                cmp.select_prev_item({behavior = 'insert'})
-              else
-                cmp.complete()
-              end
-            end),
-            ['<C-n>'] = cmp.mapping(function()
-              if cmp.visible() then
-                cmp.select_next_item({behavior = 'insert'})
-              else
-                cmp.complete()
-              end
-            end),
-          },
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-                { name = 'path' },
-            })
-        })
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = {
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+				["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+				["<C-p>"] = cmp.mapping(function()
+					if cmp.visible() then
+						cmp.select_prev_item({ behavior = "insert" })
+					else
+						cmp.complete()
+					end
+				end),
+				["<C-n>"] = cmp.mapping(function()
+					if cmp.visible() then
+						cmp.select_next_item({ behavior = "insert" })
+					else
+						cmp.complete()
+					end
+				end),
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
 
 		vim.diagnostic.config({
 			virtual_text = true,
--- lua/pecenka/plugins/luasnip.lua
+++ lua/pecenka/plugins/luasnip.lua
@@ -1,24 +1,43 @@
 return {
- "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets" },
+	"L3MON4D3/LuaSnip",
+	dependencies = { "rafamadriz/friendly-snippets" },
 	-- follow latest release.
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
-    config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+	config = function()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        local ls = require("luasnip")
+		local ls = require("luasnip")
 
-        vim.keymap.set({"i"}, "<C-L>", function() ls.expand() end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump( 1) end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<C-K>", function() ls.jump(-1) end, {silent = true})
+		vim.keymap.set({ "i" }, "<C-L>", function()
+			ls.expand()
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-J>", function()
+			ls.jump(1)
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-K>", function()
+			ls.jump(-1)
+		end, { silent = true })
 
-        vim.keymap.set({"i", "s"}, "<C-E>", function()
-            if ls.choice_active() then
-                ls.change_choice(1)
-            end
-        end, {silent = true})
-        require("luasnip/loaders/from_vscode").load()
-    end,
+		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
+		require("luasnip/loaders/from_vscode").load()
+
+		-- local s = ls.snippet
+		-- local t = ls.text_node
+		-- local i = ls.insert_node
+		--
+		-- ls.add_snippets("rust", {
+		--     s("pp", {
+		--         t("println!("),
+		--         t("\""),
+		--         i(1), -- This creates a placeholder for the cursor.
+		--         t("\");"),
+		--     }),
+		-- })
+	end,
 }
