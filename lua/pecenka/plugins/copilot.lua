740cfc471dbab99c36703c2d31c4567ca9285e80
Jakub Pečenka <pecenja2@fit.cvut.cz>
2025-01-11 19:18:21 +0100

    feat: copilot toggle

--- lua/pecenka/plugins/copilot.lua
+++ lua/pecenka/plugins/copilot.lua
@@ -1,12 +1,17 @@
 return {
-  "github/copilot.vim",
+	"github/copilot.vim",
 
-  config = function()
-    vim.g.copilot_filetypes = {markdown = true, yaml = true, json = true, toml = true }
-
-    -- vim.g.copilot_no_tab_map = true
-    -- vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  end
+	config = function()
+		vim.g.copilot_filetypes = { markdown = true, yaml = true, json = true, toml = true }
+		vim.keymap.set("n", "<leader>§§", function()
+			local output = vim.api.nvim_exec("Copilot status", {})
+      if output == "Copilot: Ready" then
+        vim.cmd("Copilot disable")
+        print("Copilot disabled")
+      else
+        vim.cmd("Copilot enable")
+        print("Copilot enabled")
+      end
+		end)
+	end,
 }
-
-
