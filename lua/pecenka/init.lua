84e150d2db8c74a0471e4126ecc13a779d65d021
Jakub Pečenka <pecenja2@fit.cvut.cz>
2025-03-07 13:13:17 +0100

    feat: todo cmd

--- lua/pecenka/init.lua
+++ lua/pecenka/init.lua
@@ -49,23 +49,43 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-local function populate_quickfix_with_files()
-    local files = vim.fn.glob(vim.fn.expand("%:p:h") .. "/*", false, true)
-    vim.fn.setqflist({}, "r")
-
-    local entries = {}
-    for _, file in ipairs(files) do
-        if vim.fn.isdirectory(file) == 0 then
-            table.insert(entries, { filename = file })
-        end
-    end
-    vim.fn.setqflist(entries, "r")
-    vim.cmd("copen")
-end
-
 vim.api.nvim_create_user_command(
     "PopulateQuickfix",
-    populate_quickfix_with_files,
+    function ()
+      local files = vim.fn.glob(vim.fn.expand("%:p:h") .. "/*", false, true)
+      vim.fn.setqflist({}, "r")
+
+      local entries = {}
+      for _, file in ipairs(files) do
+          if vim.fn.isdirectory(file) == 0 then
+              table.insert(entries, { filename = file })
+          end
+      end
+      vim.fn.setqflist(entries, "r")
+      vim.cmd("copen")
+    end,
     { desc = "Populate quickfix list with files in current directory" }
 )
 
+vim.api.nvim_create_user_command(
+  "TodoFile",
+  function()
+    -- get root directory of neovim project
+    local root = vim.fn.getcwd()
+
+    -- if TODO.md exists, open it
+    -- otherwise, create it and open it
+
+    local todo_file = root .. "/TODO.md"
+
+    if vim.fn.filereadable(todo_file) == 1 then
+      vim.cmd("e " .. todo_file)
+    else
+      vim.cmd("e " .. todo_file)
+      vim.fn.append(0, "# TODO")
+      vim.cmd("w")
+    end
+
+  end,
+  { desc = "Open TODO.md file." }
+)
