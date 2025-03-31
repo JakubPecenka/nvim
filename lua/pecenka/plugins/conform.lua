921199f0d973793298872cc06a3270d30c9d2024
Jakub Pečenka <pecenja2@fit.cvut.cz>
2025-03-31 20:45:42 +0200

    format

--- lua/pecenka/plugins/conform.lua
+++ lua/pecenka/plugins/conform.lua
@@ -1,31 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				python = { "isort", "black" },
-				lua = { "stylua" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				cpp = { "clang-format" },
-				c = { "clang-format" },
-				rust = { "rustfmt" },
-				go = { "gofumpt", "goimports" },
-			},
-			rules = {
-				{
-					width = 80,
-				},
-			},
-		})
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        python = { "isort", "black" },
+        lua = { "stylua" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+        rust = { "rustfmt" },
+        go = { "gofumpt", "goimports" },
+      },
+      rules = {
+        {
+          width = 80,
+        },
+      },
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>=", function()
-			require("conform").format({
-				lsp_format = "fallback",
-				timeout = 10000,
-				async = true,
-			})
-		end)
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>=", function()
+      require("conform").format({
+        lsp_format = "fallback",
+        async = true,
+      })
+    end)
+  end,
 }
